dda00fff871c6260b9bca4840309ad6246fc2ada
Abdullahkhan836 <140530087+Abdullahkhan836@users.noreply.github.com>
2023-11-11 00:59:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+//problem 1//
+// const person = {
+//     name: "Abdullah",
+//     age: 30
+//   };
+
+//   function displayPersonInfo(personObject) {
+//     console.log("Name: " + personObject.name);
+//     console.log("Age: " + personObject.age);
+//   }
+
+//   displayPersonInfo(person);
+
+//* Problem 4
+
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// console.log(fruits)
+// console.log(fruits.sort())
+
+//* Problem 6
+       
+// const input2 = prompt("Enter a word")
+// function reveseString(params) {
+//     return input2.split("").reverse().join()
+// }
+// console.log(reveseString())
+
+//* Problem 8
+
+// for(let a = 1; a <= 10 ; a++){
+//     console.log(a)
+// }
+//* Problem 10
+  
+// const input = +prompt("Enter a Number")
+// function isEven() {
+//     if(input % 2 ===0){
+//         alert('The number '+ input +' is even');
+//     }else{
+//         alert ('The number '+ input +' is odd')
+//     }
+// }
+// isEven()
+
+//* Problem 11
+// const input = prompt("Enter Your Age")
+// function checkVotingEligibility() {
+//     if (input >= 18){
+//         alert ("You are eligible to vote");
+//     } else{
+//         alert ("Sorry, you're not eligible to vote yet.");
+//     }
+// }
+//     checkVotingEligibility();
+
+//* Problem 12
+
+// function displayCurrentDateTime() {
+//     return new Date;
+// }
+// console.log(displayCurrentDateTime());
+    
+    
+
+//* Problem 16
+
+// const xyz = document.getElementById("xyz");
+// const abc = document.getElementById("abc")
+// abc.addEventListener("click",function () {
+//              xyz.innerHTML = "Button Clicked"    
+
+//* Problem 17
+
+// function displayValue() {
+//        const getValue = document.getElementById("txt").value;
+//        alert(getValue);
+     
+// }
+// //* Problem 9
+
+// function sumOfArray(arr) {
+//     let sum = 0;
+//     for(const i of arr ){
+//          sum +=i ;
+//     }
+//    return sum;
+// }
+//     console.log(sumOfArray([3,4,5]));
